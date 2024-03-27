e43e264a8f23c9aa2656671cca9270c0a4e4f415
arynch24 <arynch24@gmail.com>
2024-03-27 08:36:57 +0530

    finally done

--- Password Authenticator/pass.py
+++ Password Authenticator/pass.py
@@ -1,4 +1,12 @@
 def password_validation(pass_,user_,last_pass):
+    global count_cap
+    global count_small
+    global count_digit
+    global count_spe_cha
+    global repetition_flag
+    global hist_pass
+    global seq_flag
+    
     count_cap=0
     count_small=0
     count_digit=0
@@ -6,6 +14,7 @@ def password_validation(pass_,user_,last_pass):
     repetition_flag=0
     hist_pass=0
     seq_flag=0
+
     for i in range (0, len(pass_)):
         if(pass_[i].isupper()):
             count_cap+=1
@@ -54,6 +63,24 @@ while(ans==False):
     if(ans==True):
         print("Congratulations! Your Password is Valid.")
     else:
-        print("Retry")
+        print("Correct the following errors and retry.")
+        if(count_cap<2):
+            print("Please Enter at least 2 capital letters.")
+        if(count_small<2):
+            print("Please Enter at least 2 small letters.")
+        if(count_digit<2):
+            print("Please Enter at least 2 digits.")
+        if(count_spe_cha<2):
+            print("Please Enter at least 2 special characters.")
+        if(repetition_flag==1):
+            print("Your password contains more than 3 same characters in a sequence.")
+        if(hist_pass==1):
+            print("Your password is same as your last password.")
+        if(seq_flag==1):
+            print("Your username characters are matched with password.")
+        
+            
+
+        
 
 
